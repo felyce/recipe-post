9bc2873ead136c85361daa7668e273128806a2b1
Dai Takahashi <dai@digital-widget.com>
2021-02-12 06:03:40 +0900

    ログアウト画面を作る

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 from lib.views import IndexTemplateView
 
@@ -14,6 +14,7 @@ urlpatterns = [
     path('staffroom/', include('staffroom.urls', namespace="staffroom")),
 
     path('login', LoginView.as_view(template_name="login.html"), name="login"),
+    path('logout', LogoutView.as_view(template_name="logout.html"), name="logout"),
 
     path('', IndexTemplateView.as_view(), name="index"),
 
