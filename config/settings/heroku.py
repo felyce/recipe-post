5e73daba411c1641f7bad4b6317d8fca6f3d3e37
Dai Takahashi <dai@digital-widget.com>
2021-02-12 07:00:41 +0900

    Heroku用の設定を追加する

--- config/settings/heroku.py
+++ config/settings/heroku.py
@@ -1,6 +1,10 @@
+import os
+import dj_database_url
+
 from .common import *
 
 DEBUG = False
+ALLOWED_HOSTS = ["*", ]
 
 AWS_ACCESS_KEY_ID = "××××××××××××××××××××"
 AWS_SECRET_ACCESS_KEY = "××××××××××××××××××××××××××××××××××××××××"
@@ -11,3 +15,7 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 MEDIA_URL = S3_URL
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
