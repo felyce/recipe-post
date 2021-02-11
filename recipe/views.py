1630e8178c41c383b319b01661a87553f3b6759a
Dai Takahashi <dai@digital-widget.com>
2021-02-12 01:07:22 +0900

    メッセージで通知する

--- recipe/views.py
+++ recipe/views.py
@@ -5,6 +5,8 @@ from django.views.generic import (
 
 from django.urls import reverse, reverse_lazy
 
+from django.contrib import messages
+
 from .models import Recipe
 
 
@@ -16,6 +18,14 @@ class RecipeCreateView(CreateView):
     model = Recipe
     fields = ["title", "content", "description"]
     success_url = reverse_lazy("recipe:index")
+
+    def form_valid(self, form):
+        messages.success(self.request, "更新しました")
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "更新できませんでした")
+        return super().form_invalid(form)
 
 
 class RecipeDetailView(DetailView):
@@ -31,6 +41,19 @@ class RecipeUpdateView(UpdateView):
         pk = self.kwargs.get("pk")
         return reverse("recipe:detail", kwargs={"pk": pk})
 
+    def form_valid(self, form):
+        messages.success(self.request, "更新しました")
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "更新できませんでした")
+        return super().form_invalid(form)
+
+
 class RecipeDeleteView(DeleteView):
     model = Recipe
     success_url = reverse_lazy("recipe:index")
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, "削除しました")
+        return super().delete(request, *args, **kwargs)
