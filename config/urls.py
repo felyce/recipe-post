9a796c4270296595d6307f5d3fd2e2acc1eb4877
Dai Takahashi <dai@digital-widget.com>
2021-02-12 03:50:14 +0900

    レシピの投稿を受け付けるViewを作る

--- config/urls.py
+++ config/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('recipe/', include('recipe.urls')),
+    path('comment/', include('comment.urls')),
     path('', IndexTemplateView.as_view(), name="index"),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
