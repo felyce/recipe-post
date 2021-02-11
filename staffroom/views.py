49e6a42246a99b76f11f903591e59f8cfdd337ba
Dai Takahashi <dai@digital-widget.com>
2021-02-12 04:57:49 +0900

    ユーザーが投稿したレシピを一覧表示する

--- staffroom/views.py
+++ staffroom/views.py
@@ -1,5 +1,17 @@
 from django.views.generic import TemplateView
 
+from recipe.models import Recipe
+
 
 class StaffroomTemplateView(TemplateView):
     template_name = "staffroom/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        if self.request.user.is_authenticated:
+            context['recipe_list'] = Recipe.objects.filter(user=self.request.user)
+        else:
+            context['recipe_list'] = None
+
+        return context
