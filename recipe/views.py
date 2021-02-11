b6c636b2dcf2123b4b67f2875871cf597c3a122d
Dai Takahashi <dai@digital-widget.com>
2021-02-12 05:08:02 +0900

    編集機能をユーザーページに移動する

--- recipe/views.py
+++ recipe/views.py
@@ -1,11 +1,7 @@
 from django.views.generic import (
-    ListView, CreateView, DetailView, UpdateView,
-    DeleteView
+    ListView, DetailView
 )
 
-from django.urls import reverse, reverse_lazy
-
-from django.contrib import messages
 
 from .models import Recipe
 from comment.forms import CommentForm
@@ -24,20 +20,6 @@ class RecipeListView(ListView):
         return qs
 
 
-class RecipeCreateView(CreateView):
-    model = Recipe
-    fields = ["title", "content", "description", "image",]
-    success_url = reverse_lazy("recipe:index")
-
-    def form_valid(self, form):
-        messages.success(self.request, "更新しました")
-        return super().form_valid(form)
-
-    def form_invalid(self, form):
-        messages.error(self.request, "更新できませんでした")
-        return super().form_invalid(form)
-
-
 class RecipeDetailView(DetailView):
     model = Recipe
 
@@ -47,30 +29,3 @@ class RecipeDetailView(DetailView):
         context['CommentForm'] = CommentForm(initial={'recipe': self.object})
 
         return context
-
-
-class RecipeUpdateView(UpdateView):
-    model = Recipe
-    fields = ["title", "content", "description", "image", ]
-    # success_url = "/"
-
-    def get_success_url(self):
-        pk = self.kwargs.get("pk")
-        return reverse("recipe:detail", kwargs={"pk": pk})
-
-    def form_valid(self, form):
-        messages.success(self.request, "更新しました")
-        return super().form_valid(form)
-
-    def form_invalid(self, form):
-        messages.error(self.request, "更新できませんでした")
-        return super().form_invalid(form)
-
-
-class RecipeDeleteView(DeleteView):
-    model = Recipe
-    success_url = reverse_lazy("recipe:index")
-
-    def delete(self, request, *args, **kwargs):
-        messages.success(self.request, "削除しました")
-        return super().delete(request, *args, **kwargs)
