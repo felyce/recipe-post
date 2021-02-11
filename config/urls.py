c8da3b1e63c93e21ec4aef257352e39462cbaa15
Dai Takahashi <dai@digital-widget.com>
2021-02-12 00:54:05 +0900

    アプリケーション専用のurls.pyを作る

--- config/urls.py
+++ config/urls.py
@@ -1,38 +1,12 @@
-"""config URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from lib.views import IndexTemplateView
-from recipe.views import (
-    RecipeListView, RecipeCreateView, RecipeDetailView,
-    RecipeUpdateView, RecipeDeleteView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('recipe/', RecipeListView.as_view(), name="recipe-index"),
-    path('recipe/create', RecipeCreateView.as_view(), name="recipe-create"),
-
-    path('recipe/<int:pk>/update', RecipeUpdateView.as_view(),
-         name="recipe-update"),
-    path('recipe/<int:pk>/delete',
-         RecipeDeleteView.as_view(), name="recipe-delete"),
-    path('recipe/<int:pk>', RecipeDetailView.as_view(), name="recipe-detail"),
-
+    path('recipe/', include('recipe.urls')),
     path('', IndexTemplateView.as_view(), name="index"),
 
 ]
---
+++ recipe/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import path
+
+from recipe.views import (
+    RecipeListView, RecipeCreateView, RecipeDetailView,
+    RecipeUpdateView, RecipeDeleteView)
+
+app_name = "recipe"
+
+urlpatterns = [
+
+    path('', RecipeListView.as_view(), name="index"),
+    path('create', RecipeCreateView.as_view(), name="create"),
+
+    path('<int:pk>/update', RecipeUpdateView.as_view(),
+         name="update"),
+    path('<int:pk>/delete',
+         RecipeDeleteView.as_view(), name="delete"),
+    path('<int:pk>', RecipeDetailView.as_view(), name="detail"),
+
+]
