8b651ba13f9f5b36ad958be12ef6a197adddb114
Dai Takahashi <dai@digital-widget.com>
2021-02-12 06:28:55 +0900

    トップページにアクセスできるかテストする

--- recipe/tests.py
+++ recipe/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestRecipe(TestCase):
+
+    def test_top_page(self):
+        res = self.client.get("/")
+        self.assertEqual(res.status_code, 200)
+
+    def test_recipe_top(self):
+        res = self.client.get("/recipe/")
+        self.assertEqual(res.status_code, 200)
